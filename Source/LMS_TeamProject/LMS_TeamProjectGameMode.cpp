1f4f38fb5bab6862bb03ada929b49dd67f689225
FYTQFA <tqfa9874@gmail.com>
2026-07-01 11:32:39 +0900

    1.0
    260701_1.0ver

---
+++ Source/LMS_TeamProject/LMS_TeamProjectGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LMS_TeamProjectGameMode.h"
+#include "LMS_TeamProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ALMS_TeamProjectGameMode::ALMS_TeamProjectGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
