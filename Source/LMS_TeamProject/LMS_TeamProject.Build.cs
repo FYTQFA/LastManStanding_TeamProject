c5a37d0c367f5fa39aaa7591a4b15ded2ef6eeb1
Bosukbar <joj112601@naver.com>
2026-07-03 11:02:22 +0900

    이거 Rider로 수정후에 이런게 생김
    문제있으면 Rider는 버릴게용

--- Source/LMS_TeamProject/LMS_TeamProject.Build.cs
+++ Source/LMS_TeamProject/LMS_TeamProject.Build.cs
@@ -8,6 +8,7 @@ public class LMS_TeamProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
+			"EnhancedInput","GameplayAbilities","GameplayTasks","GameplayTags" });
 	}
 }
