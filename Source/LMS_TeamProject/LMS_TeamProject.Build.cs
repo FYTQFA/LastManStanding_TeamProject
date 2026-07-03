a1bb5c7d69885cfa0f9f411ba77d8c924fb4f64a
Bosukbar <joj112601@naver.com>
2026-07-03 15:49:37 +0900

    빌드 UMG추가

--- Source/LMS_TeamProject/LMS_TeamProject.Build.cs
+++ Source/LMS_TeamProject/LMS_TeamProject.Build.cs
@@ -9,6 +9,6 @@ public class LMS_TeamProject : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
-			"EnhancedInput","GameplayAbilities","GameplayTasks","GameplayTags" });
+			"EnhancedInput","GameplayAbilities","GameplayTasks","GameplayTags", "UMG" });
 	}
 }
