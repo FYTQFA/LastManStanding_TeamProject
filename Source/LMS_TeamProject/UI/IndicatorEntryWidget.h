e1217bc1bee45258eabc88e0cbcb76232d6f9b07
Bosukbar <joj112601@naver.com>
2026-07-08 19:56:36 +0900

    색 안되던 버그 수정

--- Source/LMS_TeamProject/UI/IndicatorEntryWidget.h
+++ Source/LMS_TeamProject/UI/IndicatorEntryWidget.h
@@ -5,6 +5,8 @@
 #include "IndicatorTypes.h"
 #include "IndicatorEntryWidget.generated.h"
 
+class UImage;
+
 // 인디케이터 하나(팀원/적/목표/핑 아이콘 하나)를 표현하는 위젯입니다.
 // 위치 갱신은 C++(UIndicatorOverlayWidget)에서 CanvasPanelSlot으로 처리하고,
 // 타입별 색상/모양 변경은 블루프린트에서 OnIndicatorDataUpdated를 구현해서 처리합니다.
@@ -21,8 +23,14 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "Indicator")
 	FLMSIndicatorScreenData IndicatorData;
 
+	// 디버그용: WBP의 IconImage와 이름이 같아야 BindWidgetOptional로 자동 연결됩니다.
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+	TObjectPtr<UImage> IconImage;
+
 protected:
 	// 타입에 따른 아이콘 색상/모양, 화살표 회전 등 실제 비주얼 처리는 블루프린트에서 구현합니다.
 	UFUNCTION(BlueprintImplementableEvent, Category = "Indicator")
 	void OnIndicatorDataUpdated();
+
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
 };
