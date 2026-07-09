bb8e10ceb11bc0d896f006633622ccdf1947c0a2
cksgns032 <cksgns032@naver.com>
2026-07-09 09:47:12 +0900

    자폭 몬스터

--- Source/LMS_TeamProject/BTTask_Attack.cpp
+++ Source/LMS_TeamProject/BTTask_Attack.cpp
@@ -38,52 +38,9 @@ EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerCom
 
 	OwningComp = &OwnerComp;
 
-	/*FOnMontageEnded EndDelegate;
-	EndDelegate.BindUObject(this, &UBTTask_Attack::OnAttackMontageEnded);
-	AnimInstance->Montage_SetEndDelegate(EndDelegate, Character->AttackMontage);*/
 
-	//return EBTNodeResult::InProgress;
+	Character->Destroy();
 
 	return EBTNodeResult::Succeeded;
 }
-
-//EBTNodeResult::Type UBTTask_Attack::AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
-//{
-//	AAIController* AIController = OwnerComp.GetAIOwner();
-//	ABaseEnemyCharacter* Character = AIController ? Cast<ABaseEnemyCharacter>(AIController->GetPawn()) : nullptr;
-//	UAnimInstance* AnimInstance = Character ? Character->GetMesh()->GetAnimInstance() : nullptr;
-//
-//	if (!AnimInstance || !Character->AttackMontage)
-//	{
-//		return EBTNodeResult::Aborted;
-//	}
-//
-//	bIsAborting = true;
-//	AnimInstance->Montage_Stop(0.f, Character->AttackMontage);
-//
-//	return EBTNodeResult::InProgress;
-//}
-//
-//void UBTTask_Attack::OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted)
-//{
-//	if (UBehaviorTreeComponent* BTComp = OwningComp.Get())
-//	{
-//		if (UBlackboardComponent* BlackboardComp = BTComp->GetBlackboardComponent())
-//		{
-//			const float MaxCoolTime = BlackboardComp->GetValueAsFloat(TEXT("MaxCoolTime"));
-//			BlackboardComp->SetValueAsFloat(TEXT("CoolTime"), MaxCoolTime);
-//			BlackboardComp->SetValueAsBool(TEXT("CanAttack"), false);
-//		}
-//
-//		if (bIsAborting)
-//		{
-//			bIsAborting = false;
-//			FinishLatentAbort(*BTComp);
-//		}
-//		else
-//		{
-//			FinishLatentTask(*BTComp, bInterrupted ? EBTNodeResult::Failed : EBTNodeResult::Succeeded);
-//		}
-//	}
-//}
 
